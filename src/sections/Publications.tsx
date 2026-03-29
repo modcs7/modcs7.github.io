894562e7df3847f0fab72288259656c31717a78a
MOHAMMED HAMZAH  ALSALIHI <62318912+modcs7@users.noreply.github.com>
2026-03-29 01:38:53 +0100

    Update Publications.tsx

--- src/sections/Publications.tsx
+++ src/sections/Publications.tsx
@@ -9,15 +9,8 @@ interface Publication {
   link?: string;
 }
 
-const publications: Publication[] = [
-    
- {
-    title: 'Sharp Coefficient Estimates for Analytic Functions Subordinate to the Cusp Domain: Theory and Image Processing Applications',
-    authors: 'El-Ityan M, Tayyah AS, Alsalihi MH, Frasin BA, Lupaş AA',
-    venue: 'Mathematics',
-    year: '2026',
-    link: 'https://www.mdpi.com/2227-7390/14/6/1075',
-  },
+const publications: Publication[] = [  
+
  {
     title: 'Face mask Detection from Speech Using Deep Speaker Embeddings',
     authors: 'M. H. Alsalihi and D. Sztahó',
